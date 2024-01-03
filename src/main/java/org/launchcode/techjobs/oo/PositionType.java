4f19f7103e34d5f9a858a838bf6346afa427b1fd
ArielleWaks <ariellemwaks@gmail.com>
2024-01-03 16:37:19 -0600

    PositionType hashCode and equals methods added

--- src/main/java/org/launchcode/techjobs/oo/PositionType.java
+++ src/main/java/org/launchcode/techjobs/oo/PositionType.java
@@ -1,5 +1,7 @@
 package org.launchcode.techjobs.oo;
 
+import java.util.Objects;
+
 public class PositionType {
 
     private int id;
@@ -16,10 +18,30 @@ public class PositionType {
         this.value = value;
     }
 
-    // TODO: Add a custom toString() method that returns the data stored in 'value'.
+    // Add a custom toString() method that returns the data stored in 'value'.
 
-    // TODO: Add custom equals and hashCode methods. Consider two PositionType objects "equal" when
+    @Override
+    public String toString() {
+        return value;
+    }
+
+
+    // Add custom equals and hashCode methods. Consider two PositionType objects "equal" when
     //  their id fields match.
+    // TODO: do equals and hashCode methods need to include value field?
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof PositionType that)) return false;
+        return getId() == that.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
 
     // Getters and Setters:
 
